9cc44d0707873d9ab22003a9e7d4d05292b0084d
Aryan <31454667+singh1aryan@users.noreply.github.com>
2024-01-06 23:12:34 -0800

    Create llama-streamlit.py

---
+++ llama-streamlit.py
@@ -0,0 +1,27 @@
+agent = OpenAIAgent.from_tools(query_engine_tools, verbose=True)
+
+
+st.header("Chat with the Streamlit docs 💬 📚")
+
+if "messages" not in st.session_state.keys(): # Initialize the chat message history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+    ]
+
+
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            history = agent.chat_history
+            response = agent.chat(prompt,history)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add res
